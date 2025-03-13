3fe1a50362337e00af8e85cd1f47b75ad3950bc4
Ivan <statkevich.ivan999@gmail.com>
2025-03-13 21:13:55 +0200

    Add CLI commands for setup process and predefined pattern selection

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import click
+
 from src import __version__
 
 
@@ -12,6 +13,38 @@ def cli():
 def version():
     """Display the current version of fis."""
     click.echo(f"fis version {__version__}")
+
+
+@cli.command()
+def start():
+    """Start the setup process."""
+    frontend = click.prompt(
+        "Choose a frontend framework",
+        type=click.Choice(["react", "nextjs"], case_sensitive=False),
+    )
+    backend_language = click.prompt(
+        "Choose a backend language",
+        type=click.Choice(["python", "javascript"], case_sensitive=False),
+    )
+    if backend_language == "python":
+        backend_framework = click.prompt(
+            "Choose a backend framework",
+            type=click.Choice(["fastapi", "flask"], case_sensitive=False),
+        )
+    if backend_language == "javascript":
+        backend_framework = click.prompt(
+            "Choose a backend framework",
+            type=click.Choice(["express", "nestjs"], case_sensitive=False),
+        )
+    print(
+        f"You chose {frontend} and {backend_language} with {backend_framework}!"
+    )
+
+
+@cli.command()
+def pattern(pattern=None):
+    """Use a predefined pattern."""
+    click.echo(f"You chose {pattern}!")
 
 
 if __name__ == "__main__":
