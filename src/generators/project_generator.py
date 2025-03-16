2d65a944ccdc625342c976d0e546537fedd0f9af
Ivan <statkevich.ivan999@gmail.com>
2025-03-16 15:15:08 +0200

    Refactor setup command in main.py to use preset option instead of pattern, enhancing project initialization with preset file validation and improved error handling.

---
+++ src/generators/project_generator.py
@@ -0,0 +1,3 @@
+def generate_project(preset: dict, name: str):
+    print(preset)
+    print(name)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import json
+import os
 from typing import Optional
 
 import click
 
 from src import __version__
+from src.generators.project_generator import generate_project
 
 
 @click.group()
@@ -19,19 +21,25 @@ def version():
 
 
 @cli.command()
-@click.option("--pattern", "-p", help="Specify the pattern name")
+@click.option("--preset", "-p", help="Specify the preset name")
 @click.option("--name", "-n", help="Specify the project name")
-def setup(pattern: Optional[str] = None, name: Optional[str] = None):
-    """Use --pattern or -p for infrastructure setup."""
-    if pattern is None:
-        click.echo("No pattern specified. Use --pattern or -p to specify a pattern.")
+def setup(preset: Optional[str] = None, name: Optional[str] = None):
+    """Use --preset or -p for infrastructure setup."""
+    if preset is None:
+        click.echo("No preset specified. Use --preset or -p to specify a preset.")
         return
     if name is None:
         click.echo("No name specified. Use --name or -n to specify a name.")
         return
 
-    with open("data.json", "r", encoding="utf-8") as file:
+    preset_file = f"src/presets/{preset}.json"
+    if not os.path.exists(preset_file):
+        click.echo(f"Preset file '{preset_file}' does not exist.")
+        return
+
+    with open(preset_file, "r", encoding="utf-8") as file:
         data = json.load(file)
+        generate_project(data, name)
 
 
 if __name__ == "__main__":
